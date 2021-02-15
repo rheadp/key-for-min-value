02dd161db779ee38b014c6cd681722584ae0709d
rheadp <rheadpea@msu.edu>
2021-02-15 11:30:15 -0500

    commit tree 91fe4fe9c35bbdc42b55cec51a325f6e5057b31c

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+    min_key = nil
+    min_value = nil
+    
+    name_hash.each do |key, value|
+        if min_value == nil
+            min_value = value
+            min_key = key
+            min_key
+        elsif
+            min_value > value
+            min_key = key
+            min_value = value
+        end
+    end
+    min_key
+    end
